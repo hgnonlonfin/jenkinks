de58454d8ec0140634ae20b43251940c05310f61
root <root@debian>
2021-11-10 16:28:36 +0100

    aventure

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Amandine!Bienvenu dans le monde informatique");
+        System.out.println("Hello, Amandine!Bienvenu dans le monde informatique. C'est pour le 22 de début de la grande aventure!");
     }
 }
